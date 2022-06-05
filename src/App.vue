d6bc769c49e4509e6f0d9c1fe8cb7a60813abdd6
Dylan N <65708398+bituq@users.noreply.github.com>
2022-06-05 14:20:58 +0200

    Add pin and snap

--- src/App.vue
+++ src/App.vue
@@ -27,143 +27,155 @@ onMounted(() => {
   
   let scrollTl = gsap.timeline({
     scrollTrigger: {
-      trigger: "#mainContainer",
+      trigger: "#pin",
       start: "center center",
-      markers: true,
       end: "+=1000",
-      scrub: 1
+      scrub: 1,
+      pin: true,
+      snap: {
+        snapTo: "labels",
+        duration: {min: .2, max: 1},
+        delay: .2,
+        ease: "expo.out"
+      }
     }
   })
 
-  scrollTl.fromTo(scrollContent.value, { opacity: 1 }, { opacity: 0 })
+  scrollTl.addLabel("start")
+    .fromTo(scrollContent.value, { opacity: 1 }, { opacity: 0 })
     .from(mainContent.value, { translateY: "25%", ease: "power3.inOut" } , "-=0.5")
     .to(explanationRef.value.querySelector(".divider"), { opacity: 0}, "=-0.5")
     .to("#leftRect", { translateY: 20, top: 0, ease: "power3.inOut" }, "-=0.5")
     .fromTo("#skillsList", { opacity: 0 }, { translateY: "-100%", opacity: 1 }, "-=.25")
     .from(".skill-item progress", { value: 0 }, "-=.5")
-    .from("#jobsList", {opacity:0, translateY: 30}, "-=.25")
+    .addLabel("skills")
+    .to("#skillsList", { opacity: 0, translateY: "-150%" }, "+=.5")
+    .fromTo("#jobsList", {opacity: 0}, { translateY: "-100%", opacity: 1 }, "-=.5")
+    .addLabel("jobs")
 })
 
 </script>
 
 <template>
   <main :data-theme="theme" class="flex justify-center w-full h-[200vh] bg-base-300">
-    <div id="mainContainer" class="fixed bg-base-100 max-w-[1080px] m-auto h-full">
-      <!-- Shapes -->
-      <div id="leftRect" class="absolute bg-primary rectangle-shape h-96 -translate-x-3/4 top-[50%] -translate-y-3/4 z-10">
-        <div class="absolute bg-circles w-full h-full z-0" />
-        <div class="py-6 px-3">
-          <ul class="text-primary-content font-semibold">
-            <li class="list-item"><span class="material-icons">email</span>zealbus@outlook.com</li>
-            <li class="list-item"><span class="material-icons">contact_page</span><a class="underline z-10" href="https://www.linkedin.com/in/dylan-noorland-56169019b/">LinkedIn Profiel</a></li>
-            <li class="list-item form-control flex-row items-center">
-              <label class="label cursor-pointer">Donker</label>
-              <input type="checkbox" class="toggle toggle-secondary z-10" v-model="themeIsLight" checked />
-              <label class="label cursor-pointer">Licht</label>
-            </li>
-          </ul>
+    <div id="mainContainer" class="fixed bg-base-100 max-w-[1080px] h-full">
+      <div id="pin">
+        <!-- Shapes -->
+        <div id="leftRect" class="absolute bg-primary rectangle-shape h-96 -translate-x-3/4 top-[50%] -translate-y-3/4 z-10">
+          <div class="absolute bg-circles w-full h-full z-0" />
+          <div class="py-6 px-3">
+            <ul class="text-primary-content font-semibold">
+              <li class="list-item"><span class="material-icons">email</span>zealbus@outlook.com</li>
+              <li class="list-item"><span class="material-icons">contact_page</span><a class="underline z-10" href="https://www.linkedin.com/in/dylan-noorland-56169019b/">LinkedIn Profiel</a></li>
+              <li class="list-item form-control flex-row items-center">
+                <label class="label cursor-pointer">Donker</label>
+                <input type="checkbox" class="toggle toggle-secondary z-10" v-model="themeIsLight" checked />
+                <label class="label cursor-pointer">Licht</label>
+              </li>
+            </ul>
+          </div>
+          <img
+            class="absolute right-0 bottom-0 object-cover h-[80%] rounded"
+            src="@/assets/img/mugshot_transparent.png"
+          />
         </div>
-        <img
-          class="absolute right-0 bottom-0 object-cover h-[80%] rounded"
-          src="@/assets/img/mugshot_transparent.png"
-        />
-      </div>
 
-      <!-- Main Content -->
-      <main ref="mainContent" class="flex flex-col h-[100vh] justify-center gap-10 text-base-content">
-        <div ref="explanationRef" class="flex flex-col gap-5 px-28">
-          <h1>Dylan Noorland</h1>
-          <p id="mainContent" class="text-xl mt-5">
-            Ik ben een tweedejaars student aan de opleiding Informatica op de Hogeschool Rotterdam. <b>Informatica is mijn passie</b>, waardoor ik snel nieuwe vaardigheden leer. Collega’s omschrijven mij als initiatiefnemend, probleemoplossend, en professioneel.
-          </p>
-          <div id="mainContent" class="divider" />
-          <h3 id="mainContent">
-            <span ref="scrollContent" class="flex items-center">
-              <span id="scrollArrowDown" class="material-icons text-5xl">
-                arrow_downward
+        <!-- Main Content -->
+        <main ref="mainContent" class="flex flex-col h-[100vh] justify-center gap-10 text-base-content">
+          <div ref="explanationRef" class="flex flex-col gap-5 px-28">
+            <h1>Dylan Noorland</h1>
+            <p id="mainContent" class="text-xl mt-5">
+              Ik ben een tweedejaars student aan de opleiding Informatica op de Hogeschool Rotterdam. <b>Informatica is mijn passie</b>, waardoor ik snel nieuwe vaardigheden leer. Collega’s omschrijven mij als initiatiefnemend, probleemoplossend, en professioneel.
+            </p>
+            <div id="mainContent" class="divider" />
+            <h3 id="mainContent">
+              <span ref="scrollContent" class="flex items-center">
+                <span id="scrollArrowDown" class="material-icons text-5xl">
+                  arrow_downward
+                </span>
+                Scroll naar beneden
               </span>
-              Scroll naar beneden
-            </span>
-          </h3>
-        </div>
-        <div id="skillsList" class="px-28">
-          <h2 class="text-center mb-2">Relatieve Ervaring</h2>
-            <div class="flex flex-row justify-evenly gap-3">
-              <div class="skill-item">
-                <img src="@/assets/img/cs.png" />
-                <h4>C#</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="5" max="5" />
-              </div>
-              <div class="skill-item">
-                <img src="@/assets/img/vue.png" />
-                <h4>Vue</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
-              </div>
-              <div class="skill-item">
-                <img src="@/assets/img/ts.png" />
-                <h4>Typescript</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
-              </div>
-              <div class="skill-item">
-                <img src="@/assets/img/lua.png" />
-                <h4>Lua</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
-              </div>
-              <div class="skill-item">
-                <img src="@/assets/img/react.png" />
-                <h4>React</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="3" max="5" />
-              </div>
-              <div class="skill-item">
-                <img src="@/assets/img/rust.png" />
-                <h4>Rust</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="2" max="5" />
-              </div>
-              <div class="skill-item">
-                <img src="@/assets/img/blazor.png" />
-                <h4>Blazor</h4>
-                <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
+            </h3>
+          </div>
+          <div id="skillsList" class="px-28">
+            <h2 class="text-center mb-2">Relatieve Ervaring</h2>
+              <div class="flex flex-row justify-evenly gap-3">
+                <div class="skill-item">
+                  <img src="@/assets/img/cs.png" />
+                  <h4>C#</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="5" max="5" />
+                </div>
+                <div class="skill-item">
+                  <img src="@/assets/img/vue.png" />
+                  <h4>Vue</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
+                </div>
+                <div class="skill-item">
+                  <img src="@/assets/img/ts.png" />
+                  <h4>Typescript</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
+                </div>
+                <div class="skill-item">
+                  <img src="@/assets/img/lua.png" />
+                  <h4>Lua</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
+                </div>
+                <div class="skill-item">
+                  <img src="@/assets/img/react.png" />
+                  <h4>React</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="3" max="5" />
+                </div>
+                <div class="skill-item">
+                  <img src="@/assets/img/rust.png" />
+                  <h4>Rust</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="2" max="5" />
+                </div>
+                <div class="skill-item">
+                  <img src="@/assets/img/blazor.png" />
+                  <h4>Blazor</h4>
+                  <progress class="absolute progress progress-secondary top-52 -rotate-90 w-40 h-4" value="4" max="5" />
+                </div>
               </div>
             </div>
+          <div id="mainContent" class="flex flex-col justify-center mt-10">
+            <div id="jobsList" class="flex flex-row gap-5 px-28">
+              <job-item :startYear="2020" :endYear="0">
+                <template #title>
+                  Oprichter
+                </template>
+                <template #place>
+                  Zeal Software Applications
+                </template>
+                <template #description>
+                  Software-ontwikkelaar, customer-support en internationale samenwerking. Meer dan <b>5,000,000 gebruikers</b> wereldwijd.
+                </template>
+              </job-item>
+              <job-item :startYear="2020" :endYear="0">
+                <template #title>
+                  Bachelor Informatica Voltijd
+                </template>
+                <template #place>
+                  Hogeschool Rotterdam
+                </template>
+                <template #description>
+                  Bij deze opleiding tot software-engineer, leer ik waardevolle lessen over <b>communicatie, software-ontwikkeling, Scrum methodologie, en software design</b>.
+                </template>
+              </job-item>
+              <job-item :startYear="2015" :endYear="2020">
+                <template #title>
+                  Havo Natuur & Techniek
+                </template>
+                <template #place>
+                  De Ring van Putten
+                </template>
+                <template #description>
+                  Ik scoorde een <b>9.9</b> voor het eindproject van het vak Informatica. Hiervoor heb ik een website gebrouwd dat geïnspireerd is door Twitter. Gemaakt in <b>PHP, javascript, en HTML</b>.
+                </template>
+              </job-item>
+            </div>
           </div>
-        <div id="mainContent" class="flex flex-col justify-center mt-10">
-          <div id="jobsList" class="flex flex-row gap-5 px-28">
-            <job-item :startYear="2020" :endYear="0">
-              <template #title>
-                Oprichter
-              </template>
-              <template #place>
-                Zeal Software Applications
-              </template>
-              <template #description>
-                Software-ontwikkelaar, customer-support en internationale samenwerking. Meer dan <b>5,000,000 gebruikers</b> wereldwijd.
-              </template>
-            </job-item>
-            <job-item :startYear="2020" :endYear="0">
-              <template #title>
-                Bachelor Informatica Voltijd
-              </template>
-              <template #place>
-                Hogeschool Rotterdam
-              </template>
-              <template #description>
-                Bij deze opleiding tot software-engineer, leer ik waardevolle lessen over <b>communicatie, software-ontwikkeling, Scrum methodologie, en software design</b>.
-              </template>
-            </job-item>
-            <job-item :startYear="2015" :endYear="2020">
-              <template #title>
-                Havo Natuur & Techniek
-              </template>
-              <template #place>
-                De Ring van Putten
-              </template>
-              <template #description>
-                Ik scoorde een <b>9.9</b> voor het eindproject van het vak Informatica. Hiervoor heb ik een website gebrouwd dat geïnspireerd is door Twitter. Gemaakt in <b>PHP, javascript, en HTML</b>.
-              </template>
-            </job-item>
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   </main>
 </template>
